2c4b61a28a9115f4f0773f517174f07caa1ad717
Jonathan Rogenes <jonathanrogenes@gmail.com>
2025-12-13 12:54:00 +0900

    list and delete function

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue'
 const name = ref('Jon Doe')
 const status = ref('active')
 const tasks = ref(['task one', 'task two', 'task three'])
+const newTask = ref('')
 
 const toggleStatus = () => {
   if (status.value === 'active') {
@@ -14,6 +15,17 @@ const toggleStatus = () => {
     status.value = 'active'
   }
 }
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value)
+    newTask.value = ''
+  }
+}
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1)
+}
 </script>
 
 <template>
@@ -22,9 +34,19 @@ const toggleStatus = () => {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="tasks">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="tasks">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
   <br />
   <button @click="toggleStatus">Change status</button>
